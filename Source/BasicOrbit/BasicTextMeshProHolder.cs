e1037636bb1785decfcbbee99af199d080d0ec21
DMagic <david.grandy@gmail.com>
2016-09-30 09:06:44 -0400

    A simple script for updating a text field

---
+++ Source/BasicOrbit/BasicTextMeshProHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BasicOrbit
+{
+	public class BasicTextMeshProHolder : MonoBehaviour
+	{
+		private TextMeshProUGUI _textmesh;
+		private Text _text;
+
+		public void Setup(TextMeshProUGUI tmp, Text t)
+		{
+			_textmesh = tmp;
+			_text = t;
+		}
+
+		private void Update()
+		{
+			if (_textmesh == null || _text == null)
+				return;
+
+			_textmesh.text = _text.text;
+		}
+	}
+}
