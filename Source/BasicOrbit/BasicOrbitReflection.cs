09aa46a084022687b86c02de1005bcb58a5910bf
DMagic <david.grandy@gmail.com>
2016-10-03 17:43:03 -0400

    Move reflection method here

---
+++ Source/BasicOrbit/BasicOrbitReflection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace BasicOrbit
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	public class BasicOrbitReflection : MonoBehaviour
+	{
+		private static bool reflectionLoaded;
+
+		private static FieldInfo orbitMarkers = null;
+
+		public static List<OrbitTargeter.Marker> GetOrbitMarkers(OrbitTargeter targeter)
+		{
+			if (targeter == null)
+				return null;
+
+			if (orbitMarkers == null)
+				return null;
+
+			try
+			{
+				return orbitMarkers.GetValue(targeter) as List<OrbitTargeter.Marker>;
+			}
+			catch (Exception e)
+			{
+				Debug.LogWarning("[Basic Orbit] Error while assigning Orbit Targeter Markers\n" + e);
+				return null;
+			}
+		}
+
+		private void Awake()
+		{
+			if (reflectionLoaded)
+				Destroy(gameObject);
+
+			orbitMarkers = typeof(OrbitTargeter).GetField("markers", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			reflectionLoaded = true;
+
+			Destroy(gameObject);
+		}
+
+	}
+}
