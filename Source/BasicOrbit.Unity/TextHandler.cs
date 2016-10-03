e5dcf582020f43c27d1e9412362725a53a768d14
DMagic <david.grandy@gmail.com>
2016-10-03 17:42:23 -0400

    New script for handling Text Mesh Pro elements

---
+++ Source/BasicOrbit.Unity/TextHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace BasicOrbit.Unity
+{
+	public class TextHandler : MonoBehaviour
+	{
+		public class OnTextEvent : UnityEvent<string> { }
+
+		private OnTextEvent _onTextUpdate = new OnTextEvent();
+
+		public UnityEvent<string> OnTextUpdate
+		{
+			get { return _onTextUpdate; }
+		}
+	}
+}
