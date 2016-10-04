c74b2ed86834c785512c5795afe275b1f0070140
DMagic <david.grandy@gmail.com>
2016-10-04 12:20:31 -0400

    License info; using references; readout accuracy

--- Source/BasicOrbit/BasicExtensions.cs
+++ Source/BasicOrbit/BasicExtensions.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿#region License
+/*
+ * Basic Orbit
+ * 
+ * BasicExtensions - A few extensions for printing values in the correct units
+ * 
+ * Copyright (C) 2016 DMagic
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * 
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,16 +34,16 @@ namespace BasicOrbit
 		public static string Distance(this double d)
 		{
 			if (d < 1000000)
-				return string.Format("{0:N0}m", d);
+				return string.Format("{0:N1}m", d);
 			else if (d < 1000000000)
-				return string.Format("{0:N1}km", d / 1000);
+				return string.Format("{0:N2}km", d / 1000);
 			else
-				return string.Format("{0:N2}Mm", d / 1000000);
+				return string.Format("{0:N3}Mm", d / 1000000);
 		}
 
 		public static string CloseDistance(this double d)
 		{
-			if (Math.Abs(d) < 1000)
+			if (Math.Abs(d) < 10000)
 			{
 				if (Math.Abs(d) > 10)
 					return string.Format("{0:N1}m", d);
@@ -28,9 +53,9 @@ namespace BasicOrbit
 					return string.Format("{0:N0}mm", d * 1000);
 			}
 			else if (d < 1000000000)
-				return string.Format("{0:N1}km", d / 1000);
+				return string.Format("{0:N2}km", d / 1000);
 			else
-				return string.Format("{0:N2}Mm", d / 1000000);
+				return string.Format("{0:N3}Mm", d / 1000000);
 		}
 
 		public static string Speed(this double d)
