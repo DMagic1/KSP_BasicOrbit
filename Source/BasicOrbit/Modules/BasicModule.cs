2ee8f371f613afced2653195bfa35ee49bf104fa
DMagic <david.grandy@gmail.com>
2016-10-04 12:18:50 -0400

    License info and using references

--- Source/BasicOrbit/Modules/BasicModule.cs
+++ Source/BasicOrbit/Modules/BasicModule.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using BasicOrbit.Unity.Unity;
+﻿#region License
+/*
+ * Basic Orbit
+ * 
+ * BasicModule - Base class for all readout modules
+ * 
+ * Copyright (C) 2016 DMagic
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * 
+ */
+#endregion
+
 using BasicOrbit.Unity.Interface;
-using UnityEngine;
 
 namespace BasicOrbit.Modules
 {
