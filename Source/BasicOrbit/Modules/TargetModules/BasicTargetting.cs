387b90473f0d07dac3976b6128a8ecb51349bf36
DMagic <david.grandy@gmail.com>
2016-10-13 13:26:10 -0400

    Unused code; unused null check

--- Source/BasicOrbit/Modules/TargetModules/BasicTargetting.cs
+++ Source/BasicOrbit/Modules/TargetModules/BasicTargetting.cs
@@ -117,8 +117,7 @@ namespace BasicOrbit.Modules.TargetModules
 				FlightGlobals.ActiveVessel.targetObject.GetOrbit() == null ||
 				FlightGlobals.ActiveVessel.targetObject.GetOrbit().referenceBody == null ||
 				FlightGlobals.ActiveVessel.orbit == null ||
-				FlightGlobals.ActiveVessel.orbit.referenceBody == null ||
-				FlightGlobals.ActiveVessel.orbit.referenceBody.orbit == null)
+				FlightGlobals.ActiveVessel.orbit.referenceBody == null)
 			{
 				_targetBody = null;
 				_vesselIntersect = false;
@@ -186,33 +185,9 @@ namespace BasicOrbit.Modules.TargetModules
 			else
 				_shipOrbit = FlightGlobals.ActiveVessel.orbit.referenceBody.orbit;
 
-			//BasicOrbit.BasicLogging("Closest Approach {0:N1}m at {1}", _targetOrbit.ClAppr, KSPUtil.PrintTime(Planetarium.GetUniversalTime() - _targetOrbit.closestTgtApprUT, 3, false));
+			Vessel.Situations sit = FlightGlobals.ActiveVessel.situation;
 
-			//if ((FlightGlobals.ActiveVessel.situation |= Vessel.Situations.LANDED | Vessel.Situations.SPLASHED | Vessel.Situations.PRELAUNCH) == 0)
-			//{
-			//	_closestDist = 0;
-			//	_closestTime = 0;
-			//	_closestRelVel = 0;
-			//}
-			//else
-			//{
-			//	if (!_isVessel && !_isCelestial)
-			//	{
-			//		_closestDist = 0;
-			//		_closestTime = 0;
-			//		_closestRelVel = 0;
-			//	}
-			//	else
-			//	{
-			//		double UT = Planetarium.GetUniversalTime();
-
-			//		_closestTime = approachTime(_trueShipOrbit, _targetOrbit, UT);
-
-			//		_closestDist = separation(_trueShipOrbit, _targetOrbit, _closestTime);
-			//	}
-			//}
-
-			if ((FlightGlobals.ActiveVessel.situation |= Vessel.Situations.LANDED | Vessel.Situations.SPLASHED | Vessel.Situations.PRELAUNCH) == 0)
+			if ((sit |= Vessel.Situations.LANDED | Vessel.Situations.SPLASHED | Vessel.Situations.PRELAUNCH) == 0)
 			{
 				_vesselIntersect = false;
 				_bodyIntersect = false;
@@ -362,67 +337,6 @@ namespace BasicOrbit.Modules.TargetModules
 					}
 				}
 			}
-		}
-
-		private static double approachTime(Orbit a, Orbit b, double UT)
-		{
-			double closestTime = UT;
-			double closest = double.MaxValue;
-			double min = UT;
-			double interval = a.period;
-
-			if (a.eccentricity > 1)
-				interval = 100 / Math.Sqrt(a.referenceBody.gravParameter / Math.Abs(Math.Pow(a.semiMajorAxis, 3)));
-
-			double max = UT + interval;
-			int divisions = 20;
-
-			for (int i = 0; i < 8; i++)
-			{
-				double dt = (max - min) / divisions;
-
-				for (int j = 0; j < divisions; j++)
-				{
-					double t = min + j * dt;
-					double dist = separation(a, b, t);
-					if (dist < closest)
-					{
-						closest = dist;
-						closestTime = t;
-					}
-				}
-				min = Clamp(closest - dt, UT, UT + interval);
-				max = Clamp(closest + dt, UT, UT + interval);
-			}
-
-
-			return closestTime;
-		}
-
-		private static double Clamp(double x, double min, double max)
-		{
-			if (x < min)
-				return min;
-			if (x > max)
-				return max;
-			return x;
-		}
-
-		private static double separation(Orbit a, Orbit b, double UT)
-		{
-			return (SwappedAbsolutePosAtUT(a, UT) - SwappedAbsolutePosAtUT(b, UT)).magnitude;
-		}
-
-		private static Vector3d SwappedAbsolutePosAtUT(Orbit a, double UT)
-		{
-			return a.referenceBody.position + SwappedRelativePosAtUT(a, UT);
-		}
-
-		private static Vector3d SwappedRelativePosAtUT(Orbit a, double UT)
-		{
-			Vector3d v = a.getRelativePositionAtUT(UT);
-
-			return new Vector3d(v.x, v.z, v.y);
 		}
 
 	}
