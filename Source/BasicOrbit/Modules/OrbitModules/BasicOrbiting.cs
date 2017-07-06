61fd4abb3cc4d260cc4193a6cd3e793549facf46
DMagic <david.grandy@gmail.com>
2017-07-06 15:25:54 -0400

    Borrow height above terrain method from @SirDiazo's Landing Height mod

--- Source/BasicOrbit/Modules/OrbitModules/BasicOrbiting.cs
+++ Source/BasicOrbit/Modules/OrbitModules/BasicOrbiting.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region License
+/*
+ * Basic Orbit
+ * 
+ * BasicOrbiting - Static class for calculating terrain height information
+ * 
+ * Copyright (C) 2016 DMagic
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * 
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BasicOrbit.Modules.OrbitModules
@@ -8,6 +34,8 @@ namespace BasicOrbit.Modules.OrbitModules
 		private static bool _updated;
 		private static double _terrainHeight;
 		private static double _radarAltitude;
+		private static List<Part> _partList;
+		private static List<KeyValuePair<Part, double>> _partsList;
 
 		public static bool Updated
 		{
@@ -42,13 +70,130 @@ namespace BasicOrbit.Modules.OrbitModules
 				_terrainHeight = FlightGlobals.ActiveVessel.PQSAltitude();
 				_radarAltitude = FlightGlobals.ActiveVessel.altitude - _terrainHeight;
 			}
-			else
+			else if (FlightGlobals.ActiveVessel.LandedOrSplashed)
 			{
 				_terrainHeight = FlightGlobals.ActiveVessel.terrainAltitude;
-				_radarAltitude = FlightGlobals.ActiveVessel.radarAltitude;
+				_radarAltitude = 0;
+			}
+			else
+			{
+				if (FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude.doubleMax(0) < 2400)
+				{
+					_terrainHeight = FlightGlobals.ActiveVessel.terrainAltitude;
+					_radarAltitude = heightToLand();
+				}
+				else
+				{
+					_terrainHeight = FlightGlobals.ActiveVessel.terrainAltitude;
+					_radarAltitude = FlightGlobals.ActiveVessel.radarAltitude;
+				}
 			}
 
 			_updated = true;
+		}
+
+		//The following methods are adapted from SirDiazo's Landing Height Mod, also release under the GPLv3:
+		//https://github.com/SirDiazo/LandingHeight
+		//They are used to calculate the distance from the lowest point on a vessel to the surface		
+		public static double heightToLand()
+		{
+			double landHeight = 0;
+			bool firstRay = true;
+
+			if (_partList == null)
+				_partList = new List<Part>();
+
+			if (_partsList == null)
+				_partsList = new List<KeyValuePair<Part, double>>();
+
+			_partList.Clear();
+
+			if (FlightGlobals.ActiveVessel.Parts.Count < 50)
+			{
+				for (int i = FlightGlobals.ActiveVessel.Parts.Count - 1; i >= 0; i--)
+				{
+					Part p = FlightGlobals.ActiveVessel.Parts[i];
+
+					if (p.collider.enabled)
+						_partList.Add(p);
+				}
+			}
+			else
+			{
+				_partsList.Clear();
+
+				for (int i = FlightGlobals.ActiveVessel.Parts.Count - 1; i >= 0; i--)
+				{
+					Part p = FlightGlobals.ActiveVessel.Parts[i];
+
+					_partsList.Add(new KeyValuePair<Part, double>(p, Vector3.SqrMagnitude(p.transform.position - FlightGlobals.ActiveVessel.mainBody.position)));
+				}
+
+				_partsList.Sort((a, b) => a.Value.CompareTo(b.Value));
+
+				for (int i = 0; i < 30; i++)
+				{
+					KeyValuePair<Part, double> pp = _partsList[i];
+
+					if (pp.Key.collider.enabled)
+						_partList.Add(pp.Key);
+				}
+			}
+
+			for (int i = _partList.Count - 1; i >= 0; i--)
+			{
+				Part p = _partList[i];
+
+				try
+				{
+					Vector3 partEdge = p.collider.ClosestPointOnBounds(FlightGlobals.currentMainBody.position);
+					RaycastHit pHit;
+					Vector3 rayDir = (FlightGlobals.currentMainBody.position - partEdge).normalized;
+					Ray pRayDown = new Ray(partEdge, rayDir);
+
+					if (Physics.Raycast(pRayDown, out pHit, (float)(FlightGlobals.ActiveVessel.mainBody.Radius + FlightGlobals.ActiveVessel.altitude), 32769))
+					{
+						if (pHit.collider.gameObject.layer == 15 || (pHit.collider.gameObject.layer == 0 && pHit.collider.gameObject.GetComponentInParent<Part>().vessel != FlightGlobals.ActiveVessel))
+						{
+							float hitDist = pHit.distance;
+
+							if (FlightGlobals.ActiveVessel.mainBody.ocean)
+							{
+								if (FlightGlobals.ActiveVessel.PQSAltitude() < 0)
+									hitDist = hitDist + (float)FlightGlobals.ActiveVessel.PQSAltitude();
+							}
+
+							if (firstRay)
+							{
+								landHeight = hitDist;
+
+								firstRay = false;
+							}
+							else if (hitDist < landHeight)
+								landHeight = hitDist;
+						}
+
+					}
+					else if (!firstRay)
+					{
+						landHeight = Math.Min(landHeight, FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude);
+						firstRay = false;
+					}
+				}
+				catch
+				{
+					landHeight = FlightGlobals.ActiveVessel.radarAltitude;
+					firstRay = false;
+				}
+			}
+
+			if (!firstRay && landHeight == 0)
+			{
+				landHeight = FlightGlobals.ActiveVessel.radarAltitude;
+				firstRay = false;
+			}
+
+			return landHeight;
 		}
 
 	}
