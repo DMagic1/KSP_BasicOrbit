771ea0463aba6986c08dfb791e61970e4f766d69
DMagic <david.grandy@gmail.com>
2017-07-06 15:20:18 -0400

    Vessel name readout

---
+++ Source/BasicOrbit/Modules/OrbitModules/VesselName.cs
@@ -0,0 +1,57 @@
+﻿#region License
+/*
+ * Basic Orbit
+ * 
+ * BasicOrbit VesselName - Readout module for the vessel name
+ * 
+ * Copyright (C) 2016 DMagic
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * 
+ */
+#endregion
+
+namespace BasicOrbit.Modules.OrbitModules
+{
+	public class VesselName : BasicModule
+	{
+		public VesselName(string t)
+			: base(t)
+		{
+
+		}
+
+		protected override void UpdateVisible()
+		{
+			BasicSettings.Instance.showVesselName = IsVisible;
+		}
+
+		protected override void UpdateAlways()
+		{
+			BasicSettings.Instance.showVesselNameAlways = AlwaysShow;
+		}
+
+		protected override string fieldUpdate()
+		{
+			if (FlightGlobals.ActiveVessel == null)
+				return "---";
+
+			if (FlightGlobals.ActiveVessel.vesselName.Length > 28)
+				return FlightGlobals.ActiveVessel.vesselName.Substring(0, 28);
+
+			return FlightGlobals.ActiveVessel.vesselName;
+		}
+	}
+}
