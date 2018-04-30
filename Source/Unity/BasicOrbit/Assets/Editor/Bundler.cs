8edb9e4ed64954309c2ac91761450434870b8a82
DMagic <david.grandy@gmail.com>
2018-04-30 11:00:51 -0400

    Asset builder script

---
+++ Source/Unity/BasicOrbit/Assets/Editor/Bundler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class Bundler
+{
+	const string dir = "AssetBundles";
+
+    [MenuItem("BasicOrbit/Build Bundles")]
+    static void BuildAllAssetBundles()
+    {
+		BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.StandaloneWindows);
+	}
+
+
+}
